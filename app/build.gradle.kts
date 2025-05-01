fb4a93a407fe0313c3a4e399119ab8d9964ccc12
humza <ahmadhumza7@gmail.com>
2025-04-30 19:42:23 -0700

    Incremented version code and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "commanderpepper.customnome"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
